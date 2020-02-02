0a75e172df29fc228d0aff1d605f1284669f65f1
Alexandre Bintz <alexandre@bintz.io>
2020-02-02 12:02:27 +0100

    remove boilerplate code

--- PianoGame/AppDelegate.swift
+++ PianoGame/AppDelegate.swift
@@ -1,26 +1,8 @@
-//
-//  AppDelegate.swift
-//  PianoGame
-//
-//  Created by Alexandre Bintz on 02/02/2020.
-//  Copyright © 2020 Alexandre Bintz. All rights reserved.
-//
 
 import Cocoa
+
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-
-
-    func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
-    }
-
-    func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
-    }
-
-
 }
-
