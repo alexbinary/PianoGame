b12fe801707f16d75af2802600f8a22fd75ebe84
Alexandre Bintz <alexandre@bintz.io>
2020-02-02 19:17:36 +0100

    basic physics with gravity and ground

--- PianoGame/Src/GameScene.swift
+++ PianoGame/Src/GameScene.swift
@@ -9,6 +9,7 @@ class GameScene: SKScene {
     var defaultCamera: SKCameraNode!
     var playerCharacter: SKShapeNode!
     var backgroundElement: SKNode!
+    var ground: SKNode!
     
     
     override func didMove(to view: SKView) {
@@ -19,9 +20,17 @@ class GameScene: SKScene {
         
         playerCharacter = SKShapeNode(rectOf: CGSize(width: 50, height: 100))
         self.addChild(playerCharacter)
+        playerCharacter.position = CGPoint(x: 0, y: 200)
+        playerCharacter.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 50, height: 100))
         
         backgroundElement = SKShapeNode(ellipseOf: CGSize(width: 50, height: 50))
         self.addChild(backgroundElement)
+        
+        ground = SKShapeNode(rectOf: CGSize(width: view.frame.width, height: view.frame.height / 2.0))
+        self.addChild(ground)
+        ground.position = CGPoint(x: 0, y: -view.frame.height / 4.0)
+        ground.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: view.frame.width, height: view.frame.height / 2.0))
+        ground.physicsBody?.isDynamic = false
     }
     
     
