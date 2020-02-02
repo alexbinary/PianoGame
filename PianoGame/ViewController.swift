041add8940ea22895445bdbf3922b1601e655402
Alexandre Bintz <alexandre@bintz.io>
2020-02-02 11:27:53 +0100

    Initial Commit

---
+++ PianoGame/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  PianoGame
+//
+//  Created by Alexandre Bintz on 02/02/2020.
+//  Copyright © 2020 Alexandre Bintz. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
