6e1c3cc43aaa783080ad3b405e96bf74b4c666a7
Alexandre Bintz <alexandre@bintz.io>
2020-02-02 12:30:41 +0100

    show simple label

--- PianoGame/Src/ViewController.swift
+++ PianoGame/Src/ViewController.swift
@@ -14,5 +14,14 @@ class ViewController: NSViewController {
         let scene = SKScene()
         
         skView.presentScene(scene)
+        
+        let label = SKLabelNode(text: "Hello")
+        
+        scene.addChild(label)
+        
+        scene.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+        scene.size = CGSize(width: 1600, height: 900)
+        
+        scene.scaleMode = .aspectFit
     }
 }
