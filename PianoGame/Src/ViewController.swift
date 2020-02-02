fd6cfbde552c366e6a6dbf82e45426f4e89ca10a
Alexandre Bintz <alexandre@bintz.io>
2020-02-02 12:18:17 +0100

    present basic scene

--- PianoGame/Src/ViewController.swift
+++ PianoGame/Src/ViewController.swift
@@ -1,7 +1,18 @@
 
 import Cocoa
+import SpriteKit
 
 
 class ViewController: NSViewController {
 
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let skView = view as! SKView
+        
+        let scene = SKScene()
+        
+        skView.presentScene(scene)
+    }
 }
