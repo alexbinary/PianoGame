9401a01326e35c31b4a8747f6cb54e7ab7e0a0db
Alexandre Bintz <alexandre@bintz.io>
2020-02-04 21:44:45 +0100

    send notes to external device

--- PianoGame/Src/ViewController.swift
+++ PianoGame/Src/ViewController.swift
@@ -1,6 +1,7 @@
 
 import Cocoa
 import SpriteKit
+import MIKMIDI
 
 
 class ViewController: NSViewController {
@@ -15,5 +16,33 @@ class ViewController: NSViewController {
         scene.scaleMode = .resizeFill
         
         skView.presentScene(scene)
+        
+        
+        guard let alesisDevice = MIKMIDIDeviceManager.shared.availableDevices.first(where: { $0.displayName == "Alesis Recital Pro " }) else {
+            fatalError("Could not find MIDI device")
+        }
+        
+        guard let alesisEntity = alesisDevice.entities.first else {
+            fatalError("MIDI device has no entity")
+        }
+        
+        guard let alesisSource = alesisEntity.sources.first else {
+            fatalError("MIDI entity has no source endpoints")
+        }
+        
+        guard let alesisDestination = alesisEntity.destinations.first else {
+            fatalError("MIDI entity has no destination endpoints")
+        }
+        
+        do {
+            try MIKMIDIDeviceManager.shared.send([
+                MIKMIDINoteOnCommand(note: 60, velocity: 64, channel: 0, timestamp: Date()),
+                MIKMIDINoteOffCommand(note: 60, velocity: 64, channel: 0, timestamp: Date().advanced(by: 0.5))
+            ], to: alesisDestination)
+        } catch {
+            fatalError("failed to send commands")
+        }
+        
+        
     }
 }
