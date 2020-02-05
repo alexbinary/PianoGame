ff8256f2963b239e28ed419f93e1993e90a5b1a7
Alexandre Bintz <alexandre@bintz.io>
2020-02-05 23:59:48 +0100

    basic game logic

--- PianoGame/Src/Quiz/QuizGameScene.swift
+++ PianoGame/Src/Quiz/QuizGameScene.swift
@@ -3,10 +3,34 @@ import Foundation
 import SpriteKit
 
 
+enum Note : CaseIterable {
+    
+    case c
+    case c_sharp
+    case d
+    case d_sharp
+    case e
+    case f
+    case f_sharp
+    case g
+    case g_sharp
+    case a
+    case a_sharp
+    case b
+}
+
+
 class QuizGameScene: SKScene {
     
     
     var labelNode: SKLabelNode!
+    
+    var currentNote: Note!
+    var currentOffset = 0
+    
+    var currentExpectedNote: Note {
+        return Note.allCases[(Note.allCases.firstIndex(of: currentNote)! + currentOffset) % Note.allCases.count]
+    }
     
     
     override func didMove(to view: SKView) {
@@ -16,5 +40,31 @@ class QuizGameScene: SKScene {
         labelNode = SKLabelNode()
         
         addChild(labelNode)
+        
+        generateNewNote()
+        updateLabel()
+    }
+    
+    
+    func generateNewNote() {
+        
+        currentNote = Note.allCases.randomElement()
+        currentOffset = Int.random(in: 1...12)
+    }
+    
+    
+    func updateLabel() {
+        
+        labelNode.text = "\(String(describing: currentNote!).uppercased()) + \(Double(currentOffset) / 2.0) tone(s)"
+    }
+    
+    
+    func onNotePlayed(_ note: Note) {
+        
+        if note == currentExpectedNote {
+            
+            generateNewNote()
+            updateLabel()
+        }
     }
 }
