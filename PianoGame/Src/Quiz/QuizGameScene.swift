963dfb685bdb3da3830da728310d8063756d3e9e
Alexandre Bintz <alexandre@bintz.io>
2020-02-05 23:49:03 +0100

    setup main label

--- PianoGame/Src/Quiz/QuizGameScene.swift
+++ PianoGame/Src/Quiz/QuizGameScene.swift
@@ -6,8 +6,15 @@ import SpriteKit
 class QuizGameScene: SKScene {
     
     
+    var labelNode: SKLabelNode!
+    
+    
     override func didMove(to view: SKView) {
         
+        self.anchorPoint = CGPoint(x: 0.5, y: 0.5)
         
+        labelNode = SKLabelNode()
+        
+        addChild(labelNode)
     }
 }
