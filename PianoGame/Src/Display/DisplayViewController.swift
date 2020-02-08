c2cbdfb10685066f764ccc9f58d6dbf4d8a49949
Alexandre Bintz <alexandre@bintz.io>
2020-02-08 12:11:40 +0100

    use MIDI file as game input

--- PianoGame/Src/Display/DisplayViewController.swift
+++ PianoGame/Src/Display/DisplayViewController.swift
@@ -7,12 +7,15 @@ import MIKMIDI
 class DisplayViewController: NSViewController {
     
     
+    var scene: DisplayGameScene!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         let skView = view as! SKView
         
-        let scene = DisplayGameScene()
+        scene = DisplayGameScene()
         scene.scaleMode = .resizeFill
         
         skView.presentScene(scene)
@@ -22,20 +25,57 @@ class DisplayViewController: NSViewController {
         
         try! MIKMIDIDeviceManager.shared.connect(alesisDevice) { (_, commands) in
                 
-            commands.forEach { command in
+            print("commands from device: \(commands)")
+            
+            self.onCommands(commands)
+        }
+        
+        let url = Bundle.main.url(forResource: "sample", withExtension: "mid")!
+        
+        let sequence = try! MIKMIDISequence(fileAt: url)
+        print("MIDI sequence has \(sequence.tracks.count) track(s)")
+        print("Track 0 has \(sequence.tracks[0].events.count) event(s): \(sequence.tracks[0].events)")
+        
+        let sequencer = MIKMIDISequencer(sequence: sequence)
+        sequencer.shouldCreateSynthsIfNeeded = false
+        sequencer.setCommandScheduler(self, for: sequence.tracks[1])
+        sequencer.setCommandScheduler(self, for: sequence.tracks[2])
+        
+        sequencer.startPlayback()
+    }
+    
+    
+    func onCommands(_ commands: [MIKMIDICommand]) {
+        
+        commands.forEach { command in
+            
+            print("now: \(Date()) - command timestamp: \(command.timestamp)")
+            
+            if let noteOnCommand = command as? MIKMIDINoteOnCommand {
                 
-                if let noteOnCommand = command as? MIKMIDINoteOnCommand {
+                if noteOnCommand.velocity > 0 {
                     
-                    if noteOnCommand.velocity > 0 {
-                        
-                        scene.noteOn(noteOnCommand.note)
+                    scene.noteOn(noteOnCommand.note)
+                
+                } else {
                     
-                    } else {
-                        
-                        scene.noteOff(noteOnCommand.note)
-                    }
+                    scene.noteOff(noteOnCommand.note)
                 }
             }
         }
+    }
+}
+
+
+extension DisplayViewController: MIKMIDICommandScheduler {
+    
+    
+    func scheduleMIDICommands(_ commands: [MIKMIDICommand]) {
+        
+        print("commands from sequencer: \(commands)")
+        
+        onCommands(commands)
+        
+        try! MIKMIDIDeviceManager.shared.send(commands, to: MIKMIDIDeviceManager.shared.availableDevices.first(where: { $0.displayName == "Alesis Recital Pro " })!.entities.first!.destinations.first!)
     }
 }
