74035034c8fdc42c726e9e4242071d436d37e576
Alexandre Bintz <alexandre@bintz.io>
2020-03-01 16:10:55 +0100

    add CGPoint += operator

--- PianoGame/Src/Operators.swift
+++ PianoGame/Src/Operators.swift
@@ -6,3 +6,10 @@ func +(lhs: CGPoint, rhs: CGPoint) -> CGPoint {
  
     CGPoint(x: lhs.x + rhs.x, y: lhs.y + rhs.y)
 }
+
+
+func +=(lhs: inout CGPoint, rhs: CGPoint) {
+ 
+    lhs.x += rhs.x
+    lhs.y += rhs.y
+}
