fdc303a81675025287a95ac96e35218f79f921ff
Alexandre Bintz <alexandre@bintz.io>
2020-03-06 17:04:41 +0100

    more experiments with puzzle design

--- PianoGame/Src/SimpleCountingGameScene/SimpleCountingGameScene.swift
+++ PianoGame/Src/SimpleCountingGameScene/SimpleCountingGameScene.swift
@@ -61,6 +61,36 @@ class SimpleCountingGameScene: SKScene {
                hiddenNoteNames: [],
                expectedNote: .a),
         
+        Puzzle(startingNote: .b,
+               visibleNotes: [ .b ],
+               hiddenNoteNames: [],
+               expectedNote: .b),
+        
+        Puzzle(startingNote: .c,
+               visibleNotes: [ .c ],
+               hiddenNoteNames: [],
+               expectedNote: .c),
+        
+        Puzzle(startingNote: .d,
+               visibleNotes: [ .d ],
+               hiddenNoteNames: [],
+               expectedNote: .d),
+        
+        Puzzle(startingNote: .e,
+               visibleNotes: [ .e ],
+               hiddenNoteNames: [],
+               expectedNote: .e),
+        
+        Puzzle(startingNote: .f,
+               visibleNotes: [ .f ],
+               hiddenNoteNames: [],
+               expectedNote: .f),
+        
+        Puzzle(startingNote: .g,
+               visibleNotes: [ .g ],
+               hiddenNoteNames: [],
+               expectedNote: .g),
+        
         Puzzle(startingNote: .a,
                visibleNotes: [ .a, .b ],
                hiddenNoteNames: [],
@@ -80,7 +110,7 @@ class SimpleCountingGameScene: SKScene {
     var playerCharacterNode: SKNode!
     
     
-    lazy var markers: [CGFloat] = { (0..<self.puzzles.count).map { CGFloat($0 * 100) } }()
+    lazy var markers: [CGFloat] = { (0..<self.puzzles.count).map { CGFloat($0 * 100 + (-50...50).randomElement()!) } }()
     
     
     override func didMove(to view: SKView) {
