4511523a8f4094314840db02da0e788b9f579e5b
Alexandre Bintz <alexandre@bintz.io>
2020-03-08 21:59:53 +0100

    AudioKit test

--- PianoGame/Src/AudioKitTest/AudioKitTestScene.swift
+++ PianoGame/Src/AudioKitTest/AudioKitTestScene.swift
@@ -8,8 +8,8 @@ class AudioKitTestScene: SKScene {
     
     
     var oscillator1 = AKOscillator()
-//    var oscillator2 = AKOscillator()
-//    var mixer = AKMixer()
+    var oscillator2 = AKOscillator()
+    var mixer = AKMixer()
     
     
     override func didMove(to view: SKView) {
@@ -27,30 +27,30 @@ class AudioKitTestScene: SKScene {
     
     func startTest() {
         
-//        mixer = AKMixer(oscillator1, oscillator2)
-//
-//        // Cut the volume in half since we have two oscillators
-//        mixer.volume = 0.5
-//        AudioKit.output = mixer
-//        do {
-//            try AudioKit.start()
-//        } catch {
-//            AKLog("AudioKit did not start!")
-//        }
+        mixer = AKMixer(oscillator1, oscillator2)
+
+        // Cut the volume in half since we have two oscillators
+        mixer.volume = 0.5
+        AudioKit.output = mixer
+        do {
+            try AudioKit.start()
+        } catch {
+            AKLog("AudioKit did not start!")
+        }
+        
+        self.toggleSound()
     }
     
     
-//    func toggleSound(_ sender: UIButton) {
-//        if oscillator1.isPlaying {
-//            oscillator1.stop()
-//            oscillator2.stop()
-//            sender.setTitle("Play Sine Waves", for: .normal)
-//        } else {
-//            oscillator1.frequency = random(in: 220 ... 880)
-//            oscillator1.start()
-//            oscillator2.frequency = random(in: 220 ... 880)
-//            oscillator2.start()
-//            sender.setTitle("Stop \(Int(oscillator1.frequency))Hz & \(Int(oscillator2.frequency))Hz", for: .normal)
-//        }
-//    }
+    func toggleSound() {
+        if oscillator1.isPlaying {
+            oscillator1.stop()
+            oscillator2.stop()
+        } else {
+            oscillator1.frequency = random(in: 220 ... 880)
+            oscillator1.start()
+            oscillator2.frequency = random(in: 220 ... 880)
+            oscillator2.start()
+        }
+    }
 }
