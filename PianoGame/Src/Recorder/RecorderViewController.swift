90d092de35377062f441795c9ec7ccca2cd31f56
Alexandre Bintz <alexandre@bintz.io>
2020-03-17 21:51:49 +0100

    recorder stub

---
+++ PianoGame/Src/Recorder/RecorderViewController.swift
@@ -0,0 +1,24 @@
+
+import UIKit
+
+
+
+class RecorderViewController: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+    
+    @IBAction func record() {
+        
+    }
+    
+    
+    @IBAction func play() {
+        
+    }
+}
