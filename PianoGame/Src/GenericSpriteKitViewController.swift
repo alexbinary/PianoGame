d86905e25c11235db72dc61f3dcadc46229222dd
Alexandre Bintz <alexandre@bintz.io>
2020-03-31 15:17:47 +0200

    set scene resolution to 1024x768

--- PianoGame/Src/GenericSpriteKitViewController.swift
+++ PianoGame/Src/GenericSpriteKitViewController.swift
@@ -9,7 +9,7 @@ class GenericSpriteKitViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let scene = SightReadingScene(size: CGSize(width: 1600, height: 900))
+        let scene = SightReadingScene(size: CGSize(width: 1024, height: 768))
         scene.scaleMode = .aspectFit
         
         (view as! SKView).presentScene(scene)
