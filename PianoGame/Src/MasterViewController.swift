e4e9d1a1fda0d6fd9117494e03e0a9a7e9aec852
Alexandre Bintz <alexandre@bintz.io>
2020-04-05 14:29:03 +0200

    show first screen by default

--- PianoGame/Src/MasterViewController.swift
+++ PianoGame/Src/MasterViewController.swift
@@ -7,70 +7,15 @@ import SpriteKit
 class MasterViewController: UITableViewController {
     
     
-    var items: [(title: String, viewControllerBuilder: () -> UIViewController)] = []
-    
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        #if targetEnvironment(macCatalyst)
-        
-        items.append((title: "Chords Play", viewControllerBuilder: {
-            
-            let vc = GenericSpriteKitViewController()
-            
-            let scene = ChordsPlayScene(size: CGSize(width: 1024, height: 768))
-            scene.scaleMode = .aspectFit
-            
-            (vc.view as! SKView).presentScene(scene)
-            
-            return vc
-        }))
-        
-        #endif
-        
-        items.append((title: "Sing", viewControllerBuilder: {
-            
-            let vc = SingViewController()
-            
-            return vc
-        }))
-        
-        items.append((title: "Chords", viewControllerBuilder: {
-            
-            let vc = GenericSpriteKitViewController()
-            
-            let scene = ChordsScene(size: CGSize(width: 1024, height: 768))
-            scene.scaleMode = .aspectFit
-            
-            (vc.view as! SKView).presentScene(scene)
-            
-            return vc
-        }))
-        
-        items.append((title: "SightReading", viewControllerBuilder: {
-            
-            let vc = GenericSpriteKitViewController()
-            
-            let scene = SightReadingScene(size: CGSize(width: 1024, height: 768))
-            scene.scaleMode = .aspectFit
-            
-            (vc.view as! SKView).presentScene(scene)
-            
-            return vc
-        }))
-    }
-    
-    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
-        return items.count
+        return SplitViewController.items.count
     }
     
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        let item = items[indexPath.row]
+        let item = SplitViewController.items[indexPath.row]
         
         let cell = UITableViewCell(style: .default, reuseIdentifier: "Cell")
         cell.textLabel?.text = item.title
@@ -81,7 +26,7 @@ class MasterViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
-        let item = items[indexPath.row]
+        let item = SplitViewController.items[indexPath.row]
         
         self.splitViewController?.showDetailViewController(item.viewControllerBuilder(), sender: nil)
     }
--- PianoGame/Src/SplitViewController.swift
+++ PianoGame/Src/SplitViewController.swift
@@ -1,5 +1,6 @@
 
-import  UIKit
+import UIKit
+import SpriteKit
 
 
 
@@ -12,6 +13,64 @@ class SplitViewController: UISplitViewController {
         if #available(iOS 13, *) {
             self.primaryBackgroundStyle = .sidebar
         }
-        self.preferredDisplayMode = .primaryOverlay
+        
+        if let item = SplitViewController.items.first {
+            self.showDetailViewController(item.viewControllerBuilder(), sender: nil)
+        }
     }
+    
+    
+    static var items: [(title: String, viewControllerBuilder: () -> UIViewController)] = {
+        
+        var items: [(title: String, viewControllerBuilder: () -> UIViewController)] = []
+        
+        #if targetEnvironment(macCatalyst)
+        
+        items.append((title: "Chords Play", viewControllerBuilder: {
+            
+            let vc = GenericSpriteKitViewController()
+            
+            let scene = ChordsPlayScene(size: CGSize(width: 1024, height: 768))
+            scene.scaleMode = .aspectFit
+            
+            (vc.view as! SKView).presentScene(scene)
+            
+            return vc
+        }))
+        
+        #endif
+        
+        items.append((title: "Sing", viewControllerBuilder: {
+            
+            let vc = SingViewController()
+            
+            return vc
+        }))
+        
+        items.append((title: "Chords", viewControllerBuilder: {
+            
+            let vc = GenericSpriteKitViewController()
+            
+            let scene = ChordsScene(size: CGSize(width: 1024, height: 768))
+            scene.scaleMode = .aspectFit
+            
+            (vc.view as! SKView).presentScene(scene)
+            
+            return vc
+        }))
+        
+        items.append((title: "SightReading", viewControllerBuilder: {
+            
+            let vc = GenericSpriteKitViewController()
+            
+            let scene = SightReadingScene(size: CGSize(width: 1024, height: 768))
+            scene.scaleMode = .aspectFit
+            
+            (vc.view as! SKView).presentScene(scene)
+            
+            return vc
+        }))
+        
+        return items
+    }()
 }
