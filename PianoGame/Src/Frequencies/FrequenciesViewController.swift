8a93c0fc74ce710b5635c6373fa2ad0fa08fcf0b
Alexandre Bintz <alexandre@bintz.io>
2020-04-07 14:56:11 +0200

    compute first fifth

--- PianoGame/Src/Frequencies/FrequenciesViewController.swift
+++ PianoGame/Src/Frequencies/FrequenciesViewController.swift
@@ -6,54 +6,85 @@ import UIKit
 class FrequenciesViewController: UIViewController {
     
     
+    let octaveRatio: Double = 2
+    let fifthRatio: Double = 3
+    let thirdRatio: Double = 5
+    
+    let frequencyRange = 27.5.Hz...4187.Hz
+    let baseFrequency = 55.Hz
+    
+    var collectedFrequencies: [(source: String, frequency: Frequency)] = []
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let octaveRatio: Double = 2
-        let fifthRatio: Double = 3
-        let thirdRatio: Double = 5
-        
-        let frequencyRange = 27.5.Hz...4187.Hz
-        let baseFrequency = 55.Hz
-        
-        var allFrequencies: Set<Frequency> = []
-        
-        print("base frequency:")
+        print("")
+        print("base frequency")
         print(Pitch(at: baseFrequency))
         
-        print("generating octaves")
-        let octaveFrequencies = self.spellFrequencies(fromBase: baseFrequency, withRatio: octaveRatio, inRange: frequencyRange)
-        octaveFrequencies.sorted().forEach { print(Pitch(at: $0)) }
+        print("")
+        print("octaves from base frequency")
+        self.printAndCollectFrequencies(fromBase: self.baseFrequency,
+                                        withRatio: self.octaveRatio,
+                                        withinRange: self.frequencyRange,
+                                        source: "base")
         
-//        print("generating fifths")
-//        for i in 0...5 {
-//
-//            let f = baseFrequency * pow(fifthRatio, Double(i))
-//            print("\(Pitch(at: f))")
-//            allFrequencies.insert(f)
-//        }
-//
-//        print("generating thirds")
-//        for i in 0...5 {
-//
-//            let f = baseFrequency * pow(thirdRatio, Double(i))
-//            print("\(Pitch(at: f))")
-//            allFrequencies.insert(f)
-//        }
-//
-//        print("all frequencies")
-//        for f in allFrequencies.sorted() {
-//            print("\(Pitch(at: f))")
-//        }
+        let firstFifthFrequency = self.baseFrequency * self.fifthRatio
+        print("")
+        print("first fifth")
+        print(Pitch(at: firstFifthFrequency))
+        
+        print("")
+        print("octaves from first fifth")
+        self.printAndCollectFrequencies(fromBase: firstFifthFrequency,
+                                        withRatio: self.octaveRatio,
+                                        withinRange: self.frequencyRange,
+                                        source: "first fifth")
+                
+        print("")
+        print("summary")
+        printCollectedFrequencies(self.collectedFrequencies)
     }
     
     
-    func spellFrequencies(fromBase baseFrequency: Frequency, withRatio ratio: Double, inRange range: ClosedRange<Frequency>) -> Set<Frequency> {
+    func spellFrequencies(fromBase baseFrequency: Frequency, withRatio ratio: Double, withinRange range: ClosedRange<Frequency>) -> Set<Frequency> {
 
         let min = log(range.lowerBound/baseFrequency, base: ratio)
         let max = log(range.upperBound/baseFrequency, base: ratio)
         
-        return Set<Frequency>((Int(min.rounded())...Int(max.rounded())).map { baseFrequency * pow(ratio, Double($0)) })
+        return Set<Frequency>((Int(min.rounded(.up))...Int(max.rounded(.down))).map { baseFrequency * pow(ratio, Double($0)) })
+    }
+    
+    
+    func printFrequencies(_ frequencies: Set<Frequency>) {
+        
+        frequencies.sorted().forEach { print(Pitch(at: $0)) }
+    }
+    
+    
+    func collectFrequencies(_ frequencies: Set<Frequency>, source: String) {
+        
+        self.collectedFrequencies.append(contentsOf: frequencies.map { (source: source, frequency: $0) })
+        
+        self.collectedFrequencies.sort(by: { $0.frequency < $1.frequency })
+    }
+    
+    
+    func printAndCollectFrequencies(fromBase baseFrequency: Frequency, withRatio ratio: Double, withinRange range: ClosedRange<Frequency>, source: String) {
+        
+        let frequencies = self.spellFrequencies(fromBase: baseFrequency, withRatio: ratio, withinRange: range)
+        self.printFrequencies(frequencies)
+        self.collectFrequencies(frequencies, source: source)
+    }
+    
+    
+    func printCollectedFrequencies(_ collectedFrequencies: [(source: String, frequency: Frequency)]) {
+        
+        collectedFrequencies.forEach {
+            
+            print("\(Pitch(at: $0.frequency)) \t \($0.source)")
+        }
     }
 }
 
