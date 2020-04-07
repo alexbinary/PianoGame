d66d27d4a40799b8d4783092edccc59ccddf1ac7
Alexandre Bintz <alexandre@bintz.io>
2020-04-07 15:30:31 +0200

    discover Pythagorean comma

--- PianoGame/Src/Frequencies/FrequenciesViewController.swift
+++ PianoGame/Src/Frequencies/FrequenciesViewController.swift
@@ -6,12 +6,12 @@ import UIKit
 class FrequenciesViewController: UIViewController {
     
     
-    let octaveRatio: Double = 2
-    let fifthRatio: Double = 3
-    let thirdRatio: Double = 5
+    let octaveFrequencyRatio: Double = 2
+    let fifthFrequencyRatio: Double = 3.0/2.0
+    let thirdFrequencyRatio: Double = 5.0/4.0
     
-    let frequencyRange = 27.5.Hz...4187.Hz
-    let baseFrequency = 55.Hz
+    let workingFrequencyRange = 27.5.Hz...4187.Hz
+    let referenceFrequency = 27.5.Hz
     
     var collectedFrequencies: [(source: String, frequency: Frequency)] = []
     
@@ -20,36 +20,47 @@ class FrequenciesViewController: UIViewController {
         super.viewDidLoad()
         
         print("")
-        print("base frequency")
-        print(Pitch(at: baseFrequency))
+        print("reference frequency")
+        print(Pitch(at: referenceFrequency))
         
         print("")
-        print("octaves from base frequency")
-        self.printAndCollectFrequencies(fromBase: self.baseFrequency,
-                                        withRatio: self.octaveRatio,
-                                        withinRange: self.frequencyRange,
-                                        source: "base")
-        
-        let firstFifthFrequency = self.baseFrequency * self.fifthRatio
-        print("")
-        print("first fifth")
-        print(Pitch(at: firstFifthFrequency))
+        print("octaves from reference frequency")
+        self.printAndCollectOctaves(fromReference: self.referenceFrequency,
+                                    withinRange: self.workingFrequencyRange,
+                                    source: "base")
         
         print("")
-        print("octaves from first fifth")
-        self.printAndCollectFrequencies(fromBase: firstFifthFrequency,
-                                        withRatio: self.octaveRatio,
-                                        withinRange: self.frequencyRange,
-                                        source: "first fifth")
-                
+        print("fifths from reference frequency")
+        self.printAndCollectFrequencyClass(fromReference: self.referenceFrequency,
+                                           withRatio: self.fifthFrequencyRatio,
+                                           withinRange: self.workingFrequencyRange,
+                                           source: "fifths",
+                                           prefix: { "fifth #\($0)" })
+        
+        for (index, fifthFrequency) in self.generateFrequencyClass(fromReference: self.referenceFrequency,
+                                                                   withRatio: self.fifthFrequencyRatio,
+                                                                   withinRange: self.workingFrequencyRange
+        ).sorted().enumerated() {
+            
+            print("")
+            print("fifth #\(index)")
+            print(Pitch(at: fifthFrequency))
+            
+            print("")
+            print("octaves from fifth #\(index)")
+            self.printAndCollectOctaves(fromReference: fifthFrequency,
+                                        withinRange: self.workingFrequencyRange,
+                                        source: "fifth #\(index)")
+        }
+        
         print("")
         print("summary")
-        printCollectedFrequencies(self.collectedFrequencies)
+        self.printCollectedFrequencies(self.collectedFrequencies)
     }
     
     
-    func spellFrequencies(fromBase baseFrequency: Frequency, withRatio ratio: Double, withinRange range: ClosedRange<Frequency>) -> Set<Frequency> {
-
+    func generateFrequencyClass(fromReference baseFrequency: Frequency, withRatio ratio: Double, withinRange range: ClosedRange<Frequency>) -> Set<Frequency> {
+        
         let min = log(range.lowerBound/baseFrequency, base: ratio)
         let max = log(range.upperBound/baseFrequency, base: ratio)
         
@@ -57,25 +68,30 @@ class FrequenciesViewController: UIViewController {
     }
     
     
-    func printFrequencies(_ frequencies: Set<Frequency>) {
+    func printFrequencies(_ frequencies: Set<Frequency>, prefix: (_ index: Int) -> String = { _ in "" }) {
         
-        frequencies.sorted().forEach { print(Pitch(at: $0)) }
+        frequencies.sorted().enumerated().forEach { print("\(prefix($0.offset)) \(Pitch(at: $0.element))") }
     }
     
     
     func collectFrequencies(_ frequencies: Set<Frequency>, source: String) {
         
         self.collectedFrequencies.append(contentsOf: frequencies.map { (source: source, frequency: $0) })
-        
         self.collectedFrequencies.sort(by: { $0.frequency < $1.frequency })
     }
     
     
-    func printAndCollectFrequencies(fromBase baseFrequency: Frequency, withRatio ratio: Double, withinRange range: ClosedRange<Frequency>, source: String) {
+    func printAndCollectFrequencyClass(fromReference baseFrequency: Frequency, withRatio ratio: Double, withinRange range: ClosedRange<Frequency>, source: String, prefix: (_ index: Int) -> String = { _ in "" }) {
         
-        let frequencies = self.spellFrequencies(fromBase: baseFrequency, withRatio: ratio, withinRange: range)
-        self.printFrequencies(frequencies)
+        let frequencies = self.generateFrequencyClass(fromReference: baseFrequency, withRatio: ratio, withinRange: range)
+        self.printFrequencies(frequencies, prefix: prefix)
         self.collectFrequencies(frequencies, source: source)
+    }
+    
+    
+    func printAndCollectOctaves(fromReference baseFrequency: Frequency, withinRange range: ClosedRange<Frequency>, source: String) {
+        
+        self.printAndCollectFrequencyClass(fromReference: baseFrequency, withRatio: self.octaveFrequencyRatio, withinRange: range, source: source)
     }
     
     
