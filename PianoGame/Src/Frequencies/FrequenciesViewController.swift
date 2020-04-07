37bcc31d9c9e0c6efb20fc3d1003751b469b5441
Alexandre Bintz <alexandre@bintz.io>
2020-04-07 14:37:22 +0200

    spell frequencies

--- PianoGame/Src/Frequencies/FrequenciesViewController.swift
+++ PianoGame/Src/Frequencies/FrequenciesViewController.swift
@@ -9,12 +9,12 @@ class FrequenciesViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
-        let baseFrequency = 55.Hz
-        
         let octaveRatio: Double = 2
         let fifthRatio: Double = 3
         let thirdRatio: Double = 5
+        
+        let frequencyRange = 27.5.Hz...4187.Hz
+        let baseFrequency = 55.Hz
         
         var allFrequencies: Set<Frequency> = []
         
@@ -22,38 +22,43 @@ class FrequenciesViewController: UIViewController {
         print(Pitch(at: baseFrequency))
         
         print("generating octaves")
-        for i in 0...7 {
-            
-            let f = baseFrequency * pow(octaveRatio, Double(i))
-            print("\(Pitch(at: f))")
-            allFrequencies.insert(f)
-        }
+        let octaveFrequencies = self.spellFrequencies(fromBase: baseFrequency, withRatio: octaveRatio, inRange: frequencyRange)
+        octaveFrequencies.sorted().forEach { print(Pitch(at: $0)) }
         
-        print("generating fifths")
-        for i in 0...5 {
-            
-            let f = baseFrequency * pow(fifthRatio, Double(i))
-            print("\(Pitch(at: f))")
-            allFrequencies.insert(f)
-        }
-        
-        print("generating thirds")
-        for i in 0...5 {
-            
-            let f = baseFrequency * pow(thirdRatio, Double(i))
-            print("\(Pitch(at: f))")
-            allFrequencies.insert(f)
-        }
-        
-        print("all frequencies")
-        for f in allFrequencies.sorted() {
-            print("\(Pitch(at: f))")
-        }
+//        print("generating fifths")
+//        for i in 0...5 {
+//
+//            let f = baseFrequency * pow(fifthRatio, Double(i))
+//            print("\(Pitch(at: f))")
+//            allFrequencies.insert(f)
+//        }
+//
+//        print("generating thirds")
+//        for i in 0...5 {
+//
+//            let f = baseFrequency * pow(thirdRatio, Double(i))
+//            print("\(Pitch(at: f))")
+//            allFrequencies.insert(f)
+//        }
+//
+//        print("all frequencies")
+//        for f in allFrequencies.sorted() {
+//            print("\(Pitch(at: f))")
+//        }
     }
     
     
-//    func spellFrequencies(fromBase baseFrequency: Frequency, withRatio ratio: Double, inRange range: ClosedRange<Frequency>) -> Set<Frequency> {
-//
-//
-//    }
+    func spellFrequencies(fromBase baseFrequency: Frequency, withRatio ratio: Double, inRange range: ClosedRange<Frequency>) -> Set<Frequency> {
+
+        let min = log(range.lowerBound.valueInHertz / baseFrequency.valueInHertz, base: ratio)
+        let max = log(range.upperBound.valueInHertz / baseFrequency.valueInHertz, base: ratio)
+        
+        return Set<Frequency>((Int(min.rounded())...Int(max.rounded())).map { baseFrequency * pow(ratio, Double($0)) })
+    }
+}
+
+
+func log(_ value: Double, base: Double) -> Double {
+    
+    return log(value) / log(base)
 }
